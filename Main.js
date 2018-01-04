b31add42d37f628525576d6645c39c023efc5c12
Bryan Tai <redhatpieman@gmail.com>
2018-01-03 16:02:55 -0800

    Person falls, Wall class created

--- Main.js
+++ Main.js
@@ -1,7 +1,9 @@
 /* Define Canvas */
 
 var stage;
-var centerX = 200;
+var CENTER_X = 200;
+var WIDTH = 400;
+var HEIGHT = 700;
 
 /* Background */
   
@@ -14,6 +16,8 @@ var bg2;
 
 var pImg = new Image();
 var player;
+var PLAYER_FALL_SPEED = 15;
+var PLAYER_HEIGHT = 100;
 var preload;
 
 var playerSprites = {
@@ -34,6 +38,37 @@ var bipSoundID = "bip";
 
 var score;
 
+/* Walls */
+var WALL_HEIGHT = 10;
+var WALL_WIDTH = 400;
+var WALL_HALF_WIDTH = 200;
+var WALL_GAP = 80;
+var WALL_RAISE_SPEED = 15;
+
+var walls = [];
+
+class Wall {
+    constructor(gapLeftX, y){
+        this.leftWall = new createjs.Bitmap(preload.getResult("wall"));
+        this.rightWall = new createjs.Bitmap(preload.getResult("wall"));
+        this.gapLeftX = gapLeftX;
+        this.gapRightX = this.gapLeftX + WALL_GAP;
+        
+        this.leftWall.x = this.gapLeftX - WALL_WIDTH;
+        this.leftWall.y = y;
+        this.rightWall.x = this.gapRightX;
+        this.rightWall.y = y;
+    }
+    moveUp(){
+        this.leftWall.y += WALL_RAISE_SPEED;
+        this.rightWall.y += WALL_RAISE_SPEED;
+    }
+    addToStage(){
+        stage.addChild(this.leftWall);
+        stage.addChild(this.rightWall);
+    }
+}
+
 function Main(){
     
     /* Link Canvas */
@@ -50,7 +85,8 @@ function Main(){
         {src: "person_idle.png", id: "idle"},
         {src: "person_left.png", id: "left"},
         {src: "person_right.png", id: "right"},
-        {src: "person_fall.png", id: "fall"}
+        {src: "person_fall.png", id: "fall"},
+        {src: "wall.png", id: "wall"}
     ]
     preload = new createjs.LoadQueue(true, "assets/");
     preload.on("complete", handleComplete);
@@ -71,26 +107,35 @@ function handleComplete(){
 function addGameView(){
     //Start player off-screen, have them fall into course
     player = new createjs.Bitmap(preload.getResult("fall"));
-    player.x = centerX - 50;
+    player.x = CENTER_X - 50;
     player.y = -100;
+    player.falling = true;
     
     
     score = new createjs.Text("0", "24px Arial", "#000000");
     score.maxWidth = 1000;  //fix for Chrome 17 
     score.x = 200; 
-    score.y = 600; 
-    
-    stage.addChild(score, player);
-    
-    /* //TODO REMOVE
+    score.y = 0; 
+
+    //TODO JUST FOR TESTING
+    for (var x = 0; x < 4; x++){
+        var testWall = new Wall(WALL_GAP * x , (x+1)*100);
+        testWall.addToStage();
+    }
+    stage.addChild(player,score);
+
+    /*//TODO REMOVE
     //for testing canvas stuff
     var circle = new createjs.Shape();
     circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
-      circle.x = 100;
-      circle.y = -20;
+      circle.x = 0;
+      circle.y = 0;
       stage.addChild(circle);
-    */
-    startGame();
+     */
+    
+    createjs.Tween.get(player)
+        .to({y:100}, 1000)
+        .call(startGame());
 }
 function startGame(){
     
@@ -100,6 +145,18 @@ function startGame(){
 //TODO
 function handleTick(event){
     if(!event.paused){
+        
+        //TODO detect collision with walls here
+        
+        if (player.falling == true && player.y < HEIGHT - PLAYER_HEIGHT){ //about the bottom
+            player.y += PLAYER_FALL_SPEED;
+        }   
+        
+        
         stage.update();
     }
+}
+
+function makeWall(gapX){
+    //TODO
 }
